13d3b4914eb43bf4164ea28a4ea143cb96d36ad0
Scott Davis <stdavis@utah.gov>
2013-03-06 09:42:05 -0700

    Fixed base map urls for 10.1 servers.

--- widgets/map/data/defaultThemeInfos.js
+++ widgets/map/data/defaultThemeInfos.js
@@ -1,57 +1,57 @@
 [{
     "label": "Terrain",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Terrain/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Terrain/MapServer",
+            "opacity": 1
+        }
+    ]
 }, {
     "label": "Hybrid",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Hybrid/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Hybrid/MapServer",
+            "opacity": 1
+        }
+    ]
 }, {
     "label": "Streets",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Vector/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Vector/MapServer",
+            "opacity": 1
+        }
+    ]
 }, {
     "label": "Imagery",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Imagery2009/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Imagery/MapServer",
+            "opacity": 1
+        }
+    ]
 }, {
     "label": "Topo",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Topo/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Topo/MapServer",
+            "opacity": 1
+        }
+    ]
 }, {
     "label": "Lite",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Lite/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Lite/MapServer",
+            "opacity": 1
+        }
+    ]
 },{
     "label": "Hillshade",
     "layers": [
-		{
-		    "url": "//mapserv.utah.gov/ArcGIS/rest/services/UtahBaseMap-Hillshade/MapServer",
-		    "opacity": 1
-		}
-	]
+        {
+            "url": "http://mapserv.utah.gov/ArcGIS/rest/services/BaseMaps/Hillshade/MapServer",
+            "opacity": 1
+        }
+    ]
 }]
