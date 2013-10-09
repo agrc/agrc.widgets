2e98ea104e418ee127c82e56fef0771a1902d4bf
steveoh <sgourley@utah.gov>
2013-10-09 16:29:43 -0600

    making tests work

--- widgets/tests/SetUpTests.js
+++ widgets/tests/SetUpTests.js
@@ -1,6 +1,10 @@
-require({
+window.dojoConfig = {
     packages: [{
         name: 'agrc',
         location: 'http://localhost:8000/'
-    }]
-});
+    },{
+        name: 'stubmodule',
+        location: 'http://localhost:8000/widgets/tests/stubmodule'
+    }],
+    has: {'dojo-undef-api': true}
+};
