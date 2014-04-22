120db41ef2d210516f7c7878ba876a3296393e2c
Scott Davis <stdavis@utah.gov>
2014-04-22 07:35:41 -0600

    Started module for interacting with the web API.
    Implemented search method.
    
    More refactoring could be done when adding additional methods.

---
+++ modules/WebAPI.js
@@ -0,0 +1,98 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/request',
+    'dojo/Deferred'
+
+], function(
+    declare,
+    lang,
+    request,
+    Deferred
+) {
+    return declare(null, {
+        // description:
+        //      A convenience class for using [AGRC's web api](http://api.mapserv.utah.gov).
+
+        // baseUrl: String
+        baseUrl: 'http://api.mapserv.utah.gov/api/v1/',
+
+
+        // Properties to be sent into constructor
+
+        // apiKey: String
+        //      web api key (http://developer.mapserv.utah.gov/AccountAccess)
+        apiKey: null,
+
+
+        constructor: function (params) {
+            // summary:
+            //      description
+            // params: {}
+            console.log('agrc/modules/WebAPI::constructor', arguments);
+        
+            lang.mixin(this, params);
+        },
+        search: function (featureClass, returnValues, options) {
+            // summary:
+            //      search service wrapper (http://api.mapserv.utah.gov/#search)
+            // featureClass: String
+            //      Fully qualified feature class name eg: SGID10.Boundaries.Counties
+            // returnValues: String[]
+            //      A comma separated string of attributes to return eg: NAME,FIPS. 
+            //      To include the geometry use the shape@ token or if you want the 
+            //      envelope use the shape@envelope token.
+            // options.predicate: String
+            //      Search criteria for finding specific features in featureClass. 
+            //      Any valid ArcObjects where clause will work. If omitted, a TSQL * 
+            //      will be used instead. eg: NAME LIKE 'K%'
+            // options.geometry: String (not fully implemented)
+            //      The point geometry used for spatial queries. Points are denoted as 
+            //      'point:[x,y]'.
+            // options.spatialReference: String (not implemented)
+            // options.tolerance: Number (not implemented)
+            // options.spatialRelation: String (default: 'intersect') (not fully implemented)
+            // options.buffer: Number
+            //      A distance in meters to buffer the input geometry. 
+            //      2000 meters is the maximum buffer.
+            // pageSize: Number (not implemented)
+            // skip: Number (not implemented)
+            //
+            // returns: Promise
+            console.log('agrc/modules/WebAPI::search', arguments);
+        
+            var def = new Deferred();
+            var url = this.baseUrl + 'search/' + featureClass + '/' + returnValues.join(',');
+
+            if (!options) {
+                options = {};
+            }
+            options.apiKey = this.apiKey;
+            var params = {
+                query: options,
+                handleAs: 'json',
+                headers: {
+                    // remove the pre-flight request which breaks the request
+                    // ref: http://www.sitepen.com/blog/2014/01/15/faq-cors-with-dojo/
+                    'X-Requested-With': null
+                }
+            };
+
+            request(url, params).then(function (response) {
+                if (response.status === 200) {
+                    def.resolve(response.result);
+                } else {
+                    def.reject(response.message);
+                }
+            }, function (err) {
+                if (err.message) {
+                    def.reject(err.message);
+                } else {
+                    def.reject('Error with search request');
+                }
+            });
+
+            return def.promise;
+        }
+    });
+});
---
+++ modules/tests/spec/SpecWebAPI.js
@@ -0,0 +1,107 @@
+require([
+    'dojo/Deferred',
+
+    'agrc/modules/WebAPI',
+
+    'stubmodule'
+
+], function(
+    Deferred,
+
+    ClassUnderTest,
+
+    stubmodule
+) {
+    describe('modules/WebAPI', function() {
+        var testObject;
+        var apiKey = 'someKey';
+
+        afterEach(function() {
+            if (testObject) {
+                if (testObject.destroy) {
+                    testObject.destroy();
+                }
+
+                testObject = null;
+            }
+        });
+
+        beforeEach(function() {
+            testObject = new ClassUnderTest({apiKey: apiKey});
+        });
+
+        describe('Sanity', function() {
+            it('should create a WebAPI', function() {
+                expect(testObject).toEqual(jasmine.any(ClassUnderTest));
+            });
+        });
+
+        describe('search', function () {
+            var xhrDef;
+            var requestSpy;
+            var name;
+            var rValues;
+            var geo;
+            var testObject2;
+            var def;
+
+            beforeEach(function (done) {
+                xhrDef = new Deferred();
+                requestSpy = jasmine.createSpy('requestSpy').and.returnValue(xhrDef);
+                name = 'name';
+                rValues = ['one', 'two'];
+                geo = 'point:[x,y]';
+                stubmodule('agrc/modules/WebAPI', {
+                    'dojo/request': requestSpy
+                }).then(function (StubbedModule) {
+                    testObject2 = new StubbedModule({apiKey: apiKey});
+
+                    done();
+                });
+            });
+            it('makes an appropriately formatted request', function () {
+                def = testObject2.search(name, rValues, {geometry: geo});
+                var args = requestSpy.calls.mostRecent().args;
+
+                // url
+                expect(args[0]).toEqual(testObject2.baseUrl + 'search/' + name + '/one,two');
+
+                // data
+                expect(args[1].query.geometry).toEqual(geo);
+            });
+            it('resolves with the data', function (done) {
+                def = testObject2.search(name, rValues, {geometry: geo});
+                var d = [];
+                xhrDef.resolve({result: d, status: 200});
+                def.then(function (data) {
+                    expect(data).toBe(d);
+
+                    done();
+                });
+            });
+            it('throws errors', function (done) {
+                def = testObject2.search(name, rValues, {geometry: geo});
+                var e = 'error!';
+                xhrDef.resolve({
+                    status: 'not 200',
+                    message: e
+                });
+                def.then(function () {}, function (err) {
+                    expect(err).toEqual(e);
+                    done();
+                });
+            });
+            it('adds the apiKey', function () {
+                // no options
+                def = testObject2.search(name, rValues);
+
+                expect(requestSpy.calls.mostRecent().args[1].query.apiKey).toEqual(apiKey);
+
+                // with options
+                def = testObject2.search(name, rValues, {});
+
+                expect(requestSpy.calls.mostRecent().args[1].query.apiKey).toEqual(apiKey);
+            });
+        });
+    });
+});
