e24715a98a29e639a554dc2aa36a9a3b334eecac
stdavis <stdavis@utah.gov>
2018-07-18 10:04:34 -0600

    Remove sauce labs and replace with upgraded grunt-contrib-jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,9 @@
 module.exports = function (grunt) {
-    var osx = 'OS X 10.10';
-    var windows = 'Windows 8.1';
-    var browsers = [{
-
-        // OSX
-        browserName: 'safari',
-        platform: osx
-    }, {
-
-
-        // Windows
-        browserName: 'firefox',
-        platform: windows
-    }, {
-        browserName: 'chrome',
-        platform: windows,
-        version: '47'
-    }, {
-        browserName: 'internet explorer',
-        platform: windows,
-        version: '11'
-    }, {
-        browserName: 'internet explorer',
-        platform: 'Windows 8',
-        version: '10'
-    }, {
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9'
-    }];
-
-    var sauceConfig = {
-        urls: ['http://127.0.0.1:8000/_SpecRunner.html'],
-        tunnelTimeout: 120,
-        build: process.env.TRAVIS_JOB_ID,
-        browsers: browsers,
-        testname: 'agrc-widgets',
-        maxRetries: 10,
-        maxPollRetries: 10,
-        'public': 'public',
-        throttled: 3,
-        sauceConfig: {
-            'max-duration': 10800
-        },
-        statusCheckAttempts: 500
-    };
-    var secrets;
     var bumpFiles = [
         'package.json',
+        'package-lock.json',
         'bower.json'
     ];
-    try {
-        secrets = grunt.file.readJSON('secrets.json');
-        sauceConfig.username = secrets.sauce_name;
-        sauceConfig.key = secrets.sauce_key;
-    } catch (e) {
-        // swallow for build server
-    }
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -107,7 +54,6 @@ module.exports = function (grunt) {
                         'bower_components/jasmine-jsreporter/jasmine-jsreporter.js',
                         'widgets/tests/SetUpTests.js',
                         'bower_components/dojo/dojo.js',
-                        'widgets/tests/jsReporterSanitizer.js',
                         'widgets/tests/jasmineAMDErrorChecking.js'
                     ],
                     specs: [
@@ -133,11 +79,6 @@ module.exports = function (grunt) {
             }
         },
         pkg: grunt.file.readJSON('package.json'),
-        'saucelabs-jasmine': {
-            all: {
-                options: sauceConfig
-            }
-        },
         watch: {
             files: [
                 '**',
@@ -174,15 +115,10 @@ module.exports = function (grunt) {
         'watch'
     ]);
 
-    grunt.registerTask('sauce', [
-        'jasmine:main:build',
-        'connect',
-        'saucelabs-jasmine'
-    ]);
-
     grunt.registerTask('travis', [
         'if-missing:esri_slurp:travis',
         'eslint',
-        'sauce'
+        'connect',
+        'jasmine'
     ]);
 };
